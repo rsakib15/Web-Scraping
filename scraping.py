d9387409cb391fdf54885f5bdbeb875148fa93a8
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-12 08:21:11 +0600

    Initial Commit

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://www.yellowpages.com/search?search_terms=cofee&geo_location_terms=Los+Angeles%2C+CA" 	#put your desired web-address
+url = "http://www.yellowpages.com/search?search_terms=cofee&geo_location_terms=Los+Angeles%2C+CA"  # put your desired web-address
 r = requests.get(url);
 
 print(r.content)
@@ -31,3 +31,12 @@ for item in g_data:
 
 for item in g_data:
 	print(item.contents[0].text)
+	print(item.contents[1].text)
+
+for item in g_data:
+	print(item.contents[0].find_all("a", {"class": "business-name"})[0].text)
+	print(item.contents[1].find_all("p", {"class": "adr"})[0].text)
+	try:
+		print(item.contents[1].find_all("div", {"class": "primary"})[0].text)
+	except:
+		pass
