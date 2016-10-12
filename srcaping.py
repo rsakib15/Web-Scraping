07b8d66d8ec1d27d3b28087d0cdc4d9b81b5c315
SAKIB RAHMAN <srskb15@gmail.com>
2016-10-12 07:40:43 +0600

    Initial Commit

---
+++ srcaping.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = "http://www.yellowpages.com/search?search_terms=cofee&geo_location_terms=Los+Angeles%2C+CA"
+r = requests.get(url);
+
+# print(r.content)
+
+soup = BeautifulSoup(r.content)
+# print(soup)
+
+links = soup.find_all("a")
+
+for link in links:
+	print(link.get("href"))
+
+for link in links:
+	print(link.text)
+
+for link in links:
+	print(link.text, link.get("href"))
+
+for link in links:
+	print("<a href='%s'>%s</a>" % (link.get("href"), link.text))
+
+g_data = soup.find_all("div", {"class": "info"})
+print(g_data)
+
+for item in g_data:
+	print(item.text)
+
+for item in g_data:
+	print(item.contents[0].text)
